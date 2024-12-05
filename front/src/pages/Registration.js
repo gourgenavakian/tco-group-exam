4c226df830e6c33d7fabe456d473f0f2655dd495
Manvel77 <man.vardanyan2005@gmail.com>
2024-12-05 18:25:08 +0400

    manul

--- front/src/pages/Registration.js
+++ front/src/pages/Registration.js
@@ -17,9 +17,7 @@ function Reg(props) {
         state: '',
         creditCardType: '',
         cvc: '',
-        expirationDate: '',
-
-
+        expirationDate: ''
     });
 
     const handleNext = () => {
