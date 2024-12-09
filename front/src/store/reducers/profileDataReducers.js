3a899ddeaab63e67dbabae0dabc598d5db400137
gourgenavakian <gosha.avakian98@gmail.com>
2024-12-09 05:30:41 +0400

    data in redux

--- front/src/store/reducers/profileDataReducers.js
+++ front/src/store/reducers/profileDataReducers.js
@@ -1,7 +1,7 @@
 import { FETCH_DATA_REQUEST, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE } from "../actions/profileDataActions";
 
 const initialState = {
-    data: null,
+    data: [],
     error: null,
 };
 
